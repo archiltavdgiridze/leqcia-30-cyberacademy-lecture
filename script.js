a1d9035501912c47eccd5dab1e7e5878322b7243
Archil Tavdgiridze <achitavdgiridze@gmail.com>
2024-01-11 19:57:52 +0400

    first commit for lecture 30

---
+++ script.js
@@ -0,0 +1,79 @@
+// var pirveliCifri = 14;
+// var meoreCifri = 5; 
+
+// var jami = pirveliCifri + meoreCifri
+
+// console.log(jami);
+
+// document.getElementsByClassName("demo")[2].innerHTML = jami;
+
+// მიმატება
+let mimateba1 = 34;
+let plus = "+";
+let mimateba2 = 17;
+let mimatebaJami = mimateba1 + mimateba2;
+
+document.getElementById("mimateba1").innerHTML = mimateba1;
+document.getElementById("plus").innerHTML = plus;
+document.getElementById("mimateba2").innerHTML = mimateba2;
+document.getElementById("mimatebaJami").innerHTML = mimatebaJami;
+
+// გამოკლება
+let saklebi = 67;
+let minus = "-";
+let maklebi = 24;
+let sxvaoba = saklebi - maklebi;
+
+document.getElementById("saklebi").innerHTML = saklebi;
+document.getElementById("minus").innerHTML = minus;
+document.getElementById("maklebi").innerHTML = maklebi;
+document.getElementById("sxvaoba").innerHTML = sxvaoba;
+
+// გამრავლება
+let samravli = 3;
+let gamravleba = "*";
+let mamravli = 6;
+let namravli = samravli * mamravli;
+
+document.getElementById("samravli").innerHTML = samravli;
+document.getElementById("gamravleba").innerHTML = gamravleba;
+document.getElementById("mamravli").innerHTML = mamravli;
+document.getElementById("namravli").innerHTML = namravli;
+
+
+// გაყოფა
+let gasayofi = 36;
+let gayofa = "/";
+let gamyofi = 6;
+let ganayofi = gasayofi / gamyofi;
+
+document.getElementById("gasayofi").innerHTML = gasayofi;
+document.getElementById("gayofa").innerHTML = gayofa;
+document.getElementById("gamyofi").innerHTML = gamyofi;
+document.getElementById("ganayofi").innerHTML = ganayofi;
+
+// ხარისხში აყვანა
+let asayvaniRicxvi = 4;
+let xarisxisNishani = "^";
+let xarisxi = 6;
+let pasuxi = asayvaniRicxvi ** xarisxi;
+
+
+document.getElementById("asayvaniRicxvi").innerHTML = asayvaniRicxvi;
+document.getElementById("xarisxisNishani").innerHTML = xarisxisNishani;
+document.getElementById("xarisxi").innerHTML = xarisxi;
+document.getElementById("pasuxi").innerHTML = pasuxi;
+
+
+// ფესვის ამოღება
+let fudze = 125;
+let fesvisNishani = "?";
+let fesvisXarisxi = 3
+let pasuxiFesvi = Math.cbrt(fudze);
+
+console.log(pasuxiFesvi);
+
+document.getElementById("fudze").innerHTML = fudze;
+document.getElementById("fesvisNishani").innerHTML = fesvisNishani;
+document.getElementById("fesvisXarisxi").innerHTML = fesvisXarisxi;
+document.getElementById("pasuxiFesvi").innerHTML = pasuxiFesvi;
